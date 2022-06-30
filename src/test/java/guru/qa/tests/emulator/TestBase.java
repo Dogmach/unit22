ddca42b5ff78904ea0c219409bfe8c07fd1d56d4
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-06-30 18:43:50 +0400

    Добавил файлы для эмулятора

--- src/test/java/guru/qa/tests/emulator/TestBase.java
+++ src/test/java/guru/qa/tests/emulator/TestBase.java
@@ -1,15 +1,15 @@
-package guru.qa.tests;
+package guru.qa.tests.emulator;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import guru.qa.drivers.BrowserstackMobileDriver;
+import guru.qa.drivers.EmulatorMobileDriver;
 import guru.qa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static guru.qa.helpers.Attach.sessionId;
@@ -21,7 +21,7 @@ public class TestBase {
     public static void setup() {
         addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.browser = EmulatorMobileDriver.class.getName();
         Configuration.browserSize = null;
     }
 
@@ -32,13 +32,9 @@ public class TestBase {
 
     @AfterEach
     public void afterEach() {
-        String sessionId = sessionId();
-
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
 
         step("Close driver", Selenide::closeWebDriver);
-
-        Attach.video(sessionId);
     }
 }
